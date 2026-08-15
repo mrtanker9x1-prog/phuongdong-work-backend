c6cc76ca2e30fab08eb32ac394bcf8815e26b8a0
mrtanker9x1-prog <mrtanker9x1@gmail.com>
2026-08-15 14:39:39 +0700

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Optional, Any, Dict
 from datetime import datetime
 
 # ----------------- TAG SCHEMAS -----------------
